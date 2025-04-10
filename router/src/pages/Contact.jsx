63865e56b64416bed5e7ab52679da1413aa89722
Marcelo <mcepeda@primerosystems.com>
2025-04-10 16:16:21 -0300

    Add react project

---
+++ router/src/pages/Contact.jsx
@@ -0,0 +1,5 @@
+function Contact() {
+    return <h2>📞 Contactanos</h2>;
+  }
+  export default Contact;
+  
---
+++ router/src/pages/Home.jsx
@@ -0,0 +1,5 @@
+function Home() {
+    return <h2>🏠 Bienvenido al Home</h2>;
+  }
+  export default Home;
+  
